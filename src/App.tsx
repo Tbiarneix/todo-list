72bc0317a32e5a456edca915369b87bd7cf84908
Tbiarneix <thomas.biarneix@gmail.com>
2023-08-22 11:12:24 +0200

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Todo List App</h1>
+      </div>
+      <div className="list">
+        <h2>Add a new todo task</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
