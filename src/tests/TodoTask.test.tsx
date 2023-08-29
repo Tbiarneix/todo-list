14a749b57d3900ddc39200f49c9c2c80a45baf62
Thomas <thomas.biarneix@gmail.com>
2023-08-29 19:53:29 +0200

    fix TodoTask.test mocked data

--- src/tests/TodoTask.test.tsx
+++ src/tests/TodoTask.test.tsx
@@ -1,25 +1,33 @@
 import { BrowserRouter } from "react-router-dom"
 
-import { expect, test } from "vitest"
+import { expect, test, vi } from "vitest"
 import { render, screen } from "@testing-library/react"
 
 import TodoTask from "../components/TodoTask/TodoTask.js"
 
-const taskData = {
-  id: 1,
-  title: "Create front app repository",
-  description: "To begin create a repository which will contain the front app",
-  created_at: "2023-10-28T10:00:00.000Z",
-  updated_at: "2023-08-28T11:00:00.000Z",
-  complete: true,
-}
+const setTaskListMock = vi.fn()
+const taskData = [
+  {
+    id: "1",
+    title: "Create front app repository",
+    description:
+      "To begin create a repository which will contain the front app",
+    created_at: "2023-10-28T10:00:00.000Z",
+    updated_at: "2023-08-28T11:00:00.000Z",
+    complete: true,
+  },
+]
 
 test("if TodoTask render Task title correctly", () => {
   render(
     <BrowserRouter>
-      <TodoTask task={taskData} />
+      <TodoTask
+        task={taskData[0]}
+        taskList={taskData}
+        setTaskList={setTaskListMock}
+      />
     </BrowserRouter>,
   )
-  const taskTitle = screen.getByText(taskData.title)
+  const taskTitle = screen.getByText(taskData[0].title)
   expect(taskTitle).toBeInTheDocument()
 })
